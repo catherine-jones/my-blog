c52c800e4998af855ec2f5b868da82592c1a5f93
catherine <catherine.jones41@gmail.com>
2014-02-23 13:56:14 +1100

    Ruby structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = '9a1d32d08a3b6097ffd9502f0a1533601cbc519740beca7082175044cd484684fc5a9661f1c1508884165b1cbeb3a4b67bb79cebc760927687190d9e3ba4704f'
