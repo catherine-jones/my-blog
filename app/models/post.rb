cf78a01484097a40e01b837aac8341524c1c3acc
catherine <catherine.jones41@gmail.com>
2014-03-27 19:10:13 +1100

    updates for deployment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 
   attr_accessible :body, :title, :picture, :tag_list
+  
   acts_as_taggable_on :tags
 
   validates_presence_of :body, :title
