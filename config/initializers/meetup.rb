3a1c96305591663efafe491474cc85665eb46ba4
catherine <catherine.jones41@gmail.com>
2014-04-01 19:55:16 +1100

    added inquiry form

---
+++ config/initializers/meetup.rb
@@ -0,0 +1,7 @@
+#RubyMeetup::ApiKeyClient.key = ENV['MEETUP_APIKEY']
+##json_s = client.get_path("/2/events", {:member_id => 390230} {:rsvp => "yes"})
+#request url
+#https://api.meetup.com/2/events?&sign=true&rsvp=yes&member_id=80708842&page=20
+#
+#signedurl
+#http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&rsvp=yes&desc=false&member_id=80708842&offset=0&format=json&page=20&fields=&sig_id=80708842&sig=793aae30816c3c46268941a405abd104d40d8317
