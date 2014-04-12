9a7aa8cc742b52e15efb804f3ea32a2f51d0bd3d
Simon Hildebrandt <simonhildebrandt@gmail.com>
2014-04-12 17:12:20 +1000

    Refactored meetup-api and tweaked configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :collect_meetup_events #before filter calls a function with the name that matches the symbol
+
+  def collect_meetup_events
+  	@meetup_api = ::MeetupApi.new
+  end
 end
